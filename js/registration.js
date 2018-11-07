8e686fbea30d25b341881c51bc2f75c1ab9ac9c5
sravan7 <esd14i017@iiitdm.ac.in>
2018-11-07 22:19:43 +0530

    'service'

--- js/registration.js
+++ js/registration.js
@@ -1,9 +1,9 @@
-console.log(navigator.serviceWorker)
+//console.log(navigator.serviceWorker)
 if(navigator.serviceWorker){
     navigator.serviceWorker.register('/sw.js').then(function(event) {
-       console.log('success'+event.scope); 
+       console.log('success  s'+event.scope);
     }).catch(function(event){
-        console.log('failed'+event.scope,ServiceWorkerRegistration.scope);
+        console.log('failed  '+event.scope);
     });
 
 }
--- sw.js
+++ sw.js
@@ -1,3 +1,48 @@
-self.addEventListener('fetch', function(event){
-   console.log('i am registered', event.request); 
+let presentCache = 'restaurant-v2';
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(presentCache).then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/restaurant.html',
+                'js/main.js',
+                'js/dbhelper.js',
+                'js/registration.js',
+                'js/restaurant_info.js',
+                'css/styles.css',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg'
+            ]);
+        })
+
+    );
+});
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheNames) {
+                    return cacheNames.startsWith('restaurant-') && cacheNames != presentCache;
+                }).map(function (cacheNames) {
+                    return cache.delete(cacheNames)
+                })
+            );
+        })
+    );
+});
+self.addEventListener('fetch', function(event) {
+   event.respondWith(
+   caches.match(event.request).then(function(response){
+        if(response) return response;
+       return fetch(event.request);
+   })
+   );
 });
